23baacf027154a6801c691622ddb1bc7739efe99
Tiago Santos <tiago881@gmail.com>
2021-05-04 10:41:45 -0300

    implementa setup de eventos

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+const handleSearchEvent = () => {
+  const searchInput = document.querySelector('#currency-input');
+  const currency = searchInput.value.toUpperCase();
+};
+
 const setupEvents = () => {
   const searchButton = document.querySelector('#search-button');
-  searchButton.addEventListener('click', () => {
-      //pesquisar na api usando o texto do input
-  });
+  searchButton.addEventListener('click', handleSearchEvent);
 };
 
 window.onload = () => {
