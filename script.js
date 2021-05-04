5c6c9bcc34b3926d13063ae263dca5f7c3802170
Tiago Santos <tiago881@gmail.com>
2021-05-04 12:10:33 -0300

    cria estrutura para buscar dados na API

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
+const fetchCurrency = (currency) => {
+  const endpoint = `https://api.ratesapi.io/api/latest?base=${currency}`;
+
+  fetch(endpoint)
+    .then((response) => response.json())
+    .then((obj) => console.log(obj))
+
+    .catch((error) => {});
+
+  //tratar os resultados da requisição
+};
+
 const handleSearchEvent = () => {
   const searchInput = document.querySelector('#currency-input');
   const currency = searchInput.value.toUpperCase();
+
+  fetchCurrency(currency);
 };
 
 const setupEvents = () => {
